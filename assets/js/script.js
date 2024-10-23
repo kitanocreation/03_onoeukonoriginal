636c9e429e808b120700ac02dee778e3dce75dcf
sugimotoshoki4959 <sugimoto.shoki.4959@members.co.jp>
2024-10-23 18:10:15 +0900

    90%完了
    残り
    ・メタディスクリプションの設定
    ・画像サイズの調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(function(){
         $('.ukonOrg_fade').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 100;
+            let offset = 0;
 
             if (scrollPos > targetOffset + offset) {
                 $(this).removeClass('ukonOrg_fade').addClass('ukonOrg_fadeIn');
@@ -17,27 +17,37 @@ $(function(){
         $('.ukonOrg_slide').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 100;
+            let offset = 0;
 
             if (scrollPos > targetOffset + offset) {
                 $(this).removeClass('ukonOrg_slide').addClass('ukonOrg_slideIn');
             }
         });
 
-        $('.ukonOrg_scale_card').each(function() {
+        $('.ukonOrg_scale_card_sp').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 100;
+            let offset = 0;
 
             if (scrollPos > targetOffset + offset) {
-                $(this).removeClass('ukonOrg_scale_card').addClass('ukonOrg_scaleUp_card');
+                $(this).removeClass('ukonOrg_scale_card_sp').addClass('ukonOrg_scaleUp_card_sp');
+            }
+        });
+
+        $('.ukonOrg_scale_card_pc').each(function() {
+            let targetOffset = $(this).offset().top;
+            let scrollPos = $(window).scrollTop() + $(window).height();
+            let offset = 0;
+
+            if (scrollPos > targetOffset + offset) {
+                $(this).removeClass('ukonOrg_scale_card_pc').addClass('ukonOrg_scaleUp_card_pc');
             }
         });
 
         $('.ukonOrg_scale_cta').each(function() {
             let targetOffset = $(this).offset().top;
             let scrollPos = $(window).scrollTop() + $(window).height();
-            let offset = 100;
+            let offset = 0;
 
             if (scrollPos > targetOffset + offset) {
                 $(this).removeClass('ukonOrg_scale_cta').addClass('ukonOrg_scaleUp_cta');
